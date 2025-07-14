cdd2bc0c6eb81522705fb90c5c9984eda88eb9cf
marcorosata <your-email@example.com>
2025-07-14 15:03:35 +0200

    fix: correct res.json monkey-patch to match Express signature and avoid type error

--- server/index.ts
+++ server/index.ts
@@ -13,9 +13,9 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   let capturedJsonResponse: Record<string, any> | undefined = undefined;
 
   const originalResJson = res.json;
-  res.json = function (bodyJson: any, ...args: any[]) {
+  res.json = function (bodyJson: any) {
     capturedJsonResponse = bodyJson;
-    return originalResJson.apply(res, [bodyJson, ...args]);
+    return originalResJson.call(res, bodyJson);
   };
 
   res.on("finish", () => {
